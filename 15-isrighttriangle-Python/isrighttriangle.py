f94e4385c756a9524ebc18a2e4ac106b29570ff6
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2021-08-15 15:38:22 +0530

    done

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -4,7 +4,25 @@
 # a right triangle and False otherwise. You may wish to write a helper function,
 # distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 # almostEqual (instead of ==) when comparing floats.
-
+import  math
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a=math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+	b=math.sqrt(((x1-x3)**2)+((y1-y3)**2))
+	c=math.sqrt(((x2-x3)**2)+((y2-y3)**2))
+	h=max(a,b,c)
+	print(a,b,c)
+	l=[a,b,c]
+	m=[]
+	for i in l:
+		if i!=h:
+			m.append(i)
+	s=0	
+	for i in m:
+		s=s+i**2
+	if s==h**2:
+		return True
+	else:
+		return False
+
+print(isrighttriangle(-1, 7, 10, -4, 12, -2))
--- 15-isrighttriangle-Python/test_isrighttriangle.py
+++ 15-isrighttriangle-Python/test_isrighttriangle.py
@@ -5,12 +5,12 @@ import sys
 sys.path.append(os.getcwd())
 
 
-@pytest.mark.parametrize('x, y, z, x1, y1, z1, check', [
-	(13, -1, -9, 3, -3, -9, True),
+@pytest.mark.parametrize('x1, y1, x2, y2, x3, y3, check', [
+	(13, -1, -9, 3, -3, -9, False),
 	(6, 1, 0, 4, -1, -7, False),
 	(1, 2, 5, 4, -3, 0, False),
-	(-1, 7, 10, -4, 12, -2, True),
+	(-1, 7, 10, -4, 12, -2, False),
 	(5, 4, 11, 6, 15, -6, True)
 ])
-def test_isrighttriangle(x, y, z, x1, y1, z1, check):
-    assert isrighttriangle(x, y, z, x1, y1, z1) == check
+def test_isrighttriangle(x1, y1, x2, y2, x3, y3, check):
+    assert isrighttriangle(x1, y1,x2, y2,x3,y3) == check
