280989f16c1573ebcc04a1128a6f90839b7d5d29
CHALUVADI PRASANTH KUMAR <chaluvadikumar97@gmail.com>
2021-08-17 23:13:46 +0530

    done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -12,7 +12,49 @@
 # [ [ 1, 2],
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
-
+import	numpy
 def ismostlymagicsquare(a):
 	# Your code goes here
-	pass
+	if	(len(a)==1)&(len(a[0])==1):
+		return	True
+	else:
+		r=0
+		c=0
+		ld=0
+		rd=0
+		t=0
+		for	i	in	a[0]:
+			t=t+i
+
+		for	i	in	a:
+			for	j	in	i:
+				r=r+j
+			if	r!=t:
+				return	False
+			r=0
+
+		n=numpy.array(a)
+		n=list(n.transpose())
+		for	i	in	n:
+			for	j	in	i:
+				c=c+j
+			if	c!=t:
+				return	False
+			c=0
+
+		p1=0
+		p2=0
+		for	i	in	range(len(a)):
+			ld=ld+a[i][i]
+
+		q2=len(a)-1
+		for	i	in	range(len(a)):
+			rd=rd+a[i][q2]
+			q2=q2-1
+
+		if	(t==ld)	&	(ld==rd):
+			return	True
+		else:
+			return	False
+
+#print(ismostlymagicsquare([[2, 7, 6], [9, 5, 1], [4, 3, 8]]))
