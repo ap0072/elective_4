46d78fb46f0cef055597dfbd978d0ca61e33b7e4
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2021-08-19 12:34:25 +0530

    done1

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -53,14 +53,41 @@ class BinaryTree(object):
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        pass
+        if  find_val==None:
+            return  False
+        
+        else:
+            temp1=self.root
+            temp2=self.root
+            try:
+                while(temp1):
+                    if  temp1.value==find_val:
+                        return  True
+                    elif    temp2.value==find_val:
+                        return  True
+                    else:
+                        temp1=temp1.left
+                        temp2=temp2.right
+            except:
+                pass
+
+            return  False
 
     def preorder_print(self, start, traversal):
         """
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
-        pass
+        temp1=self.root
+        temp2=self.root
+        try:
+            while(temp1):
+                print(temp1.value)
+                print(temp2.value)
+                temp1=temp1.left
+                temp2=temp2.right
+        except:
+            pass
 
 tree = BinaryTree(1)
 # tree.root.left = Node(2)
