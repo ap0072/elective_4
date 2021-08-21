a0f56d5c346f5359d45e66a4071ed6e624d23b05
CHALUVADI PRASANTH KUMAR <chaluvadikumar97@gmail.com>
2021-08-21 11:46:10 +0530

    done

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,23 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
-def ishappyprimenumber(n):
-    # Your code goes here
-    pass
+
+
+
+def helper(number,base):
+    """Perfect digital invariant function."""
+    total = 0
+    while number > 0:
+        total += pow(number % base, 2)
+        number = number // base
+    return total
+
+def ishappyprimenumber(number):
+    """Determine if the specified number is a happy number."""
+    seen_numbers = set()
+    while number > 1 and number not in seen_numbers:
+        seen_numbers.add(number)
+        number = helper(number,10)
+    return number == 1
+
+print(ishappyprimenumber(23))
